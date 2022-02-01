ec61ae4a43eb9dba006f40d3be7fe8aaad8b4fb1
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-01 11:53:26 +0900

    Add new registration and login function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, only: %i[new create]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def new; end
+
+  def create; end
+
+  def index; end
+
+  protected
+
+  # userカラム追加時に編集必要
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
