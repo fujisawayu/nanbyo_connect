7778be0ffd87823afdb1cb138e7eb3351fd3722b
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-02 21:57:09 +0900

    Add prohibit the change and deletion of guest users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'diseases#index'
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
