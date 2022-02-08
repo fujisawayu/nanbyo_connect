93e12ec2366d99edf39b413bd6b3a305ce1da8a0
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-08 13:51:02 +0900

    Add jquery and bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,13 @@
-const webpack = require('webpack');
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
-environment.plugins.append('ProvidePlugin-jQuery', new webpack.ProvidePlugin({jQuery: 'jquery'}));
+const webpack = require("webpack");
+environment.plugins.prepend(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery/src/jquery",
+    jQuery: "jquery/src/jquery",
+    Popper: "popper.js",
+  })
+);
 
-module.exports = environment
+module.exports = environment;
