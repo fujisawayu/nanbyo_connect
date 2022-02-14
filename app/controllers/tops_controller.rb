2b7c3e9eefe7f61d31ea4ab8688e2971f005c306
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-15 00:03:47 +0900

    Add path from top page to event registraiton

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,10 @@
 class TopsController < ApplicationController
   before_action :authenticate_user! 
   def index
+    @user = current_user
   end
 end
+
+
+#イベント表示機能
+#@articles = Article.order(created_at: :desc).limit(10)
