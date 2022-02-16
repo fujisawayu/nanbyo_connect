5667f27c6aff9e7c3a127871cbdc1e9820e92918
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-16 16:49:53 +0900

    Add age and prefecture to keys

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < ActionController::Base
 
   # userカラム追加時に編集必要
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :prefecture])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :prefecture])
   end
 end
