bf1f14dfe743910578618bc575c044722060f043
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-16 13:38:46 +0900

    Fix amount input form

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,4 +2,6 @@ class Expense < ApplicationRecord
   belongs_to :disease
   belongs_to :user
   validates :treatment_cost, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10000000 }
+  validates :drug_cost, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10000000 }
+  validates :other_cost, presence: true, numericality: { greater_than_or_equal_to: 0, less_than: 10000000 }
 end
