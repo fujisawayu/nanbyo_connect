3deac1bc8ad8adf08826d5188ec9352f328f9036
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-18 02:20:44 +0900

    Add validaation

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -4,4 +4,8 @@ class Disease < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :onsets, dependent: :destroy
   has_many :onset_users, through: :onsets, source: :user
+
+  validates :name,  presence: true
+  validates :number,  presence: true
+
 end
--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,4 +1,10 @@
 class Treatment < ApplicationRecord
   belongs_to :disease
   belongs_to :user
+
+  validates :affected_on,  presence: true
+  validates :drug_name,  presence: true
+  validates :content,  presence: true
+  validates :advice,  presence: true
+
 end
