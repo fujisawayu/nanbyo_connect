75ddd58e629ebaef630afc410be7e1c092784816
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-21 01:56:30 +0900

    Fix events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,66 +1,66 @@
 #卒業後実装予定
 
 
-# class EventsController < ApplicationController
-#   before_action :authenticate_user! 
-#   before_action :set_event, only: %i[ show edit update destroy ]
+class EventsController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_event, only: %i[ show edit update destroy ]
 
-#   def index
-#     @events = Event.all
-#   end
+  def index
+    @events = Event.all
+  end
 
-#   def show
-#   end
+  def show
+  end
 
-#   def new
-#     @event = Event.new
-#   end
+  def new
+    @event = Event.new
+  end
 
-#   def edit
-#   end
+  def edit
+  end
 
-#   def create
-#     @event = Event.new(event_params)
-#     @event.user_id = current_user.id
+  def create
+    @event = Event.new(event_params)
+    @event.user_id = current_user.id
 
-#     respond_to do |format|
-#       if @event.save
-#         format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
-#         format.json { render :show, status: :created, location: @event }
-#       else
-#         format.html { render :new, status: :unprocessable_entity }
-#         format.json { render json: @event.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to event_url(@event), notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#   def update
-#     respond_to do |format|
-#       if @event.update(event_params)
-#         format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
-#         format.json { render :show, status: :ok, location: @event }
-#       else
-#         format.html { render :edit, status: :unprocessable_entity }
-#         format.json { render json: @event.errors, status: :unprocessable_entity }
-#       end
-#     end
-#   end
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to event_url(@event), notice: "Event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-#   def destroy
-#     @event.destroy
+  def destroy
+    @event.destroy
 
-#     respond_to do |format|
-#       format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
-#       format.json { head :no_content }
-#     end
-#   end
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 
-#   private
-#     def set_event
-#       @event = Event.find(params[:id])
-#     end
+  private
+    def set_event
+      @event = Event.find(params[:id])
+    end
 
-#     def event_params
-#       params.require(:event).permit(:scheduled_on, :place, :title, :content, :user_id)
-#     end
-# end
+    def event_params
+      params.require(:event).permit(:scheduled_on, :place, :title, :content, :user_id)
+    end
+end
