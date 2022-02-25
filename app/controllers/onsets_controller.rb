f3e663b1e6cba19a081609d3bec82b7edfbd62ed
Yuki Fujisawa <fujisawa.yuki@outlook.com>
2022-02-25 18:39:43 +0900

    Fix error that two notices are displayed

--- app/controllers/onsets_controller.rb
+++ app/controllers/onsets_controller.rb
@@ -6,12 +6,12 @@ class OnsetsController < ApplicationController
 
   def create
     onset = current_user.onsets.create(disease_id: params[:disease_id])
-    redirect_to diseases_path, notice: "#{onset.disease.name}を登録しました"
+    redirect_to disease_url(onset.disease_id), notice: "#{onset.disease.name}を登録しました"
   end
 
   def destroy
     onset = current_user.onsets.find_by(id: params[:id]).destroy
-    redirect_to diseases_path, notice: "#{onset.disease.name}を登録解除しました"
+    redirect_to disease_url(onset.disease_id), notice: "#{onset.disease.name}を登録解除しました"
   end
 
 end
